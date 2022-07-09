d5f6027ab04786946928b18af0ae49c45dcc1273
Jordane <jordan2offshel@hotmaile.com>
2022-07-09 17:54:43 -0400

    Portfolio

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ tl = gsap.timeline({ defauts: { ease: "power1.out" } });
 tl.to(".header .logo a", { opacity: 1, duration: .5, y: 0, stagger: .25 });
 tl.to(".navb a", { opacity: 1, duration: .5, y: 0, stagger: .25 });
 
+tl.to(".content__title", { opacity: 1, x: "0%", duration: .5, stagger: .25 })
+tl.to(".content__description", { opacity: 1, x: "0%", duration: .5, stagger: .25 })
+tl.to(".btn__content", { opacity: 1, y: "0%", duration: .5, stagger: .25 })
 
 
 
